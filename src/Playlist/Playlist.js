fca2e60d4659df04fec933d0c5944db18f10f48c
Jake Nguyen <jakey@Jakes-Air.fios-router.home>
2020-10-22 16:06:38 -0400

    style

--- src/Playlist/Playlist.js
+++ src/Playlist/Playlist.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faHeartbeat} from '@fortawesome/free-solid-svg-icons';
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import './playlist.scss'
 
 const Playlist = (props) => {
-    const {songs} = props
+	const { songs } = props
 
-const loaded = () => (
-    <div>
-      {songs.map((song) => (
-        <article>
-          <p>{song.title}</p>
-          <p>{song.artist}</p>
-          <p>{song.time}</p>
-          <button onClick={() => {
-            props.deleteSong(song)
-          }}>X</button>
-          <FontAwesomeIcon icon={faHeartbeat}/>
-        </article>
-      ))}
-    </div>
-  );
+	const loaded = () => (
+		<div>
+			{songs.map((song) => (
+				<article key={song._id}>
+					<p>{song.title}</p>
+					<p>{song.artist}</p>
+					<p>{song.time}</p>
+					<button
+						onClick={() => {
+							props.deleteSong(song)
+						}}>
+						X
+					</button>
+					<FontAwesomeIcon icon={faHeart} />
+				</article>
+			))}
+		</div>
+	)
 
-  return songs.length > 0 ? loaded() : <h1>Loading...</h1>
-};
+	return songs.length > 0 ? loaded() : <h1>Loading...</h1>
+}
 
-export default Playlist;
+export default Playlist
